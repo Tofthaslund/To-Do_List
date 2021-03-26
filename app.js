6e0136c051a3fa0b92c26ecb2ef1bc39a4eb2cdf
Lasse Haslund <tofthaslund@gmail.com>
2021-03-26 10:56:03 +0000

    Basic selectors added in JS folder

---
+++ app.js
@@ -0,0 +1,8 @@
+//Selectors
+let todoInput = document.querySelector(".todo-input");
+let todoButton = document.querySelector(".btn");
+let todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+
+//Functions
