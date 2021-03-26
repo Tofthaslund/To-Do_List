0c7efb7adc550cfa283bbf1c3d7142e4a783e226
Lasse Haslund <tofthaslund@gmail.com>
2021-03-26 11:15:12 +0000

    added function in JS to create a Div and Li elements

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ let todoButton = document.querySelector(".btn");
 let todoList = document.querySelector(".todo-list");
 
 //Event Listeners
+todoButton.addEventListener('click', addTodo);
 
 //Functions
+
+function addTodo(event){
+    //Prevent form fromsubmitting
+    event.preventDefault();
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey there';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+};
